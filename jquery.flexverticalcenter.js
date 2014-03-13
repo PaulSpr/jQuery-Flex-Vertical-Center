8603bd8650cdb8bb914679bc5a06d2a76d8ea125
Graham Swan <thinkswan@gmail.com>
2014-03-13 09:21:53 -0600

    added options hash to function definition

--- jquery.flexverticalcenter.js
+++ jquery.flexverticalcenter.js
@@ -1,38 +1,43 @@
 /*global jQuery */
-/*!	
+/*!
 * FlexVerticalCenter.js 1.0
 *
 * Copyright 2011, Paul Sprangers http://paulsprangers.com
-* Released under the WTFPL license 
+* Released under the WTFPL license
 * http://sam.zoy.org/wtfpl/
 *
 * Date: Fri Oct 28 19:12:00 2011 +0100
 */
 (function( $ ){
-	
-	$.fn.flexVerticalCenter = function( onAttribute ) {
-	
+
+	$.fn.flexVerticalCenter = function( onAttribute, options ) {
+	  var attribute	= onAttribute || 'margin-top'; // the attribute to put the calculated value on
+	  var settings = $.extend({
+	    verticalOffset: 0,   // the number of pixels to offset the vertical alignment by
+      parentSelector: null // a selector representing the parent to vertically center this element within
+    }, options || {});
+
 		return this.each(function(){
-			var $this		= $(this);              // store the object
-			var attribute	= onAttribute || 'margin-top'; // the attribute to put the calculated value on
-        	
+			var $this		= $(this); // store the object
+
 			// recalculate the distance to the top of the element to keep it centered
 			var resizer = function () {
-				// get parent height minus own height and devide by 2
+				var parentHeight = (settings.parentSelector) ? $this.parents(settings.parentSelector).first().height() : $this.parent().height();
+
 				$this.css(
-					attribute, ( ( $this.parent().height() - $this.height() ) / 2 )
+					attribute, ( ( ( parentHeight - $this.height() ) / 2 ) + parseInt(settings.verticalOffset) )
 				);
 			};
 
 			// Call once to set.
 			resizer();
-				
-			// Call on resize. Opera debounces their resize by default. 
-      		$(window).resize(resizer);
-      		
-      		// Apply a load event to images within the element so it fires again after an image is loaded
-      		$this.find('img').load(resizer);
-      	
+
+			// Call on resize. Opera debounces their resize by default.
+      $(window).resize(resizer);
+
+      // Apply a load event to images within the element so it fires again after an image is loaded
+      $this.find('img').load(resizer);
+
 		});
 
 	};
